a1e6d7c15aab0868c8dea92fec99ceb4c5e69f41
hedj <hedj@gionee.com>
2017-02-28 15:32:50 +0800

    自定义默认构造函数

--- 4-1-Clock.cpp
+++ 4-1-Clock.cpp
@@ -11,6 +11,7 @@ using namespace std;
 class Clock{
     public:
         Clock(int NewH,int NewM,int NewS);
+        Clock(){Hour = 0;Minute = 0;Second = 0;}
         void SetTime(int NewH = 0,int NewM = 0,int NewS = 0);
         void ShowTime();
     private:
@@ -35,6 +36,7 @@ inline void Clock::ShowTime(){
 
 int main(){
     Clock myClock(0,0,0);//由于自定义了构造函数，编译器不会生成默认的构造函数；构造函数有形参，所以建立对象时必须给出初始值，不然编译报错
+    Clock c2;
     cout << "first time set and output:" << endl;
     myClock.SetTime();
     myClock.ShowTime();
