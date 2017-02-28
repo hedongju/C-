34bf5611809f26f81a14593d12910a879bf49821
hedj <hedj@gionee.com>
2017-02-28 10:53:05 +0800

    hello world

---
+++ 2_1.cpp
@@ -0,0 +1,18 @@
+/*
+ * 2_1.cpp
+ * Copyright (C) 2017 hdj <hdj@hdj-System-Product-Name>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "hello!\n";
+    cout << "welcome to C++!\n";
+    return 0;
+}
+
+
+
