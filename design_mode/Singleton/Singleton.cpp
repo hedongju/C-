9ab5004c320e6d667f84383a6da22b2d236c4bbd
hedj <dongjuyunzhonghe@126.com>
2017-03-06 23:22:20 +0800

    Singleton 懒汉式

---
+++ design_mode/Singleton/Singleton.cpp
@@ -0,0 +1,41 @@
+/*
+ * Singleton.cpp
+ * Copyright (C) 2017 hdj <hdj@hdj-System-Product-Name>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <iostream>
+using namespace std;
+
+class Singleton{
+    private:
+        Singleton(){
+            cout << "Singleton ..." << endl;
+        }
+    public:
+        static Singleton * GetInstance(){
+            if(m_Instance == NULL){
+                m_Instance = new Singleton;
+            }
+            return m_Instance;
+        }
+    private:
+        static Singleton *m_Instance;
+};
+
+Singleton *Singleton::m_Instance = NULL;
+
+int main(){
+    Singleton *p1 = Singleton::GetInstance();
+    Singleton *p2 = Singleton::GetInstance();
+    if(p1 == p2){
+        cout << "是同一个对象" << endl;
+    }
+    else{
+        cout << "不是同一个对象"<< endl;
+    }
+
+    return 0;
+}
+
